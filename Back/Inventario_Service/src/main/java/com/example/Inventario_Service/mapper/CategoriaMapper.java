c35b346db28caff01ec7b14b5d2507d896b4f911
Daniel Omar Alameda Lòpez <daoma222@gmail.com>
2024-05-06 00:21:05 -0600

    ajustes

--- Back/Inventario_Service/src/main/java/com/example/Inventario_Service/mapper/CategoriaMapper.java
+++ Back/Inventario_Service/src/main/java/com/example/Inventario_Service/mapper/CategoriaMapper.java
@@ -18,17 +18,24 @@ public class CategoriaMapper {
     private static ProductoRepository productoRepository;
 
     public static CategoriaDto mapperToCategoriaDto(Categoria categoria) {
-        List<Long> id_categorias_hijas = categoria.getCategorias_hijas().stream().
-                map(Categoria::getId_categoria).collect(Collectors.toList());
+     List<Long> id_categorias_hijas = categoria.getCategorias_hijas().stream()
+            .map(Categoria::getId_categoria)
+            .collect(Collectors.toList());
 
-        List<Long> id_productos = categoria.getProductos().stream().
-                map(Producto::getId_producto).collect(Collectors.toList());
+    List<Long> id_productos = categoria.getProductos().stream()
+            .map(Producto::getId_producto)
+            .collect(Collectors.toList());
 
-        return new CategoriaDto(categoria.getId_categoria(),
-                categoria.getNombre(), id_categorias_hijas,
-                categoria.getCategoria_padre().getId_categoria(),
-                id_productos
-        );
+    Long id_categoria_padre = null;
+    if (categoria.getCategoria_padre() != null) {
+        id_categoria_padre = categoria.getCategoria_padre().getId_categoria();
+    }
+
+    return new CategoriaDto(categoria.getId_categoria(),
+            categoria.getNombre(), id_categorias_hijas,
+            id_categoria_padre,
+            id_productos
+    );
     }
 
     public static Categoria mapperToCategoria(CategoriaDto categoriaDto) {
