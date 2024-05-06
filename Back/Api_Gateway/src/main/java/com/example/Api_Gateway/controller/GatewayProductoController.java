08e3c962d82b7ba6228b532f8d67d32031ddd8a8
Daniel Omar Alameda Lòpez <daoma222@gmail.com>
2024-05-06 15:31:51 -0600

    cambios

--- Back/Api_Gateway/src/main/java/com/example/Api_Gateway/controller/GatewayProductoController.java
+++ Back/Api_Gateway/src/main/java/com/example/Api_Gateway/controller/GatewayProductoController.java
@@ -34,9 +34,10 @@ public class GatewayProductoController {
 
     @PostMapping("/register")
     public ResponseFormat servicioRegister(@RequestBody ProductoDto productoDto, @RequestBody ProductoTallaDto productoTallaDto) throws JsonProcessingException {
+      System.out.println(productoTallaDto.toString());
         String jsonusuario = objectMapper.writeValueAsString(productoDto);
         RequestFormat requestFormat = new RequestFormat(jsonusuario, "create-product");
-        JOptionPane.showMessageDialog(null, productoTallaDto.toString());
+      
         return rabbitMQClient.sendMessageInventario(requestFormat);
     }
 
