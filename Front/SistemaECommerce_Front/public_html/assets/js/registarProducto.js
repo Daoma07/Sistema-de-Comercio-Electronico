ae80380aa73ca1f807f16a2168c94de50c4a5e23
Daniel Omar Alameda López <daoma222@gmail.com>
2024-05-05 22:03:55 -0600

    ajustes del front

--- Front/SistemaECommerce_Front/public_html/assets/js/registarProducto.js
+++ Front/SistemaECommerce_Front/public_html/assets/js/registarProducto.js
@@ -1,20 +1,42 @@
+fetch('http://192.168.100.21:8081/api/product/readTallas', {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+    .then(response => response.json())
+    .then(data => {
+        const content = JSON.parse(data.content);
+        const tallas = content.map(item => ({
+            talla_us: item.talla_us,
+            talla_mx: item.talla_mx
+        }));
+        const colores = ['Marrón', 'Negro', 'Blanco', 'Beige', 'Gris', 'Azul', 'Rojo', 'Verde', 'Amarillo', 'Naranja'];
 
-const tallas = ['TALLA 1', 'TALLA 2', 'TALLA 3'];
+        function agregarOpciones(select, opciones) {
+            opciones.forEach(opcion => {
+                const option = document.createElement('option');
+                option.value = opcion.id_talla;
+                option.textContent = opcion.talla_mx;
+                select.appendChild(option);
+            });
+        }
 
-// Función para agregar opciones a un select
-function agregarOpciones(select, opciones) {
-    opciones.forEach(opcion => {
-        const option = document.createElement('option');
-        option.value = opcion;
-        option.textContent = opcion;
-        select.appendChild(option);
+        function agregarColores(select, opciones) {
+            opciones.forEach(opcion => {
+                const option = document.createElement('option');
+                option.value = opcion;
+                option.textContent = opcion;
+                select.appendChild(option);
+            });
+        }
+
+        const tallaSelect = document.querySelector('select[name="talla"]');
+        const colorSelect = document.querySelector('select[name="color"]');
+        agregarOpciones(tallaSelect, tallas);
+        agregarColores(colorSelect, colores);
+    })
+    .catch(error => {
+        alert(error);
+        console.error('Error al enviar los datos:', error);
     });
-}
-
-// Selecciona los elementos select y agrega opciones
-document.addEventListener('DOMContentLoaded', function () {
-
-    const tallaSelect = document.querySelector('select[name="Talla"]');
-
-    agregarOpciones(tallaSelect, tallas);
-});
